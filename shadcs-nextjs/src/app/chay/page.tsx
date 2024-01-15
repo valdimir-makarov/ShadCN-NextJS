c096b8f8964b145e0fb1069f388ab4b9f798adab
valdimir-makarov <2010049@student.ruet.ac.bd>
2024-01-15 22:06:03 +0600

    the last Commit

---
+++ shadcs-nextjs/src/app/chay/page.tsx
@@ -0,0 +1,12 @@
+
+import { Button } from '@/components/ui/button';
+import React from 'react'
+
+function page1() {
+  return (
+    <div>page <Button>  the Mirage</Button></div>
+  
+  )
+}
+
+export default page1;
